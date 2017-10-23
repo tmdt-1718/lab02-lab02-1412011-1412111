2f191c0f109744fe6f05e65361a2f0057a16d5e5
Viet Duong <nguyenvietduongus@gmail.com.com>
2017-10-23 21:40:14 +0700

    login-register update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,9 @@ class UsersController < ApplicationController
   def index
   
   end
-
+  def show
+      @user = User.find_by id: params[:id]
+    end
   def new
     @user = User.new
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'sessions/new'
 
-	resources :users
+  get     "login"    => "sessions#new"
+  post    "login"    => "sessions#create"
+  delete  "logout"   => "sessions#destroy"
+  resources :users
 end
